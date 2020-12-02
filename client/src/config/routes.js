c7b2baae6275ac195c5ba4fd478ca6cf57f03d16
chriszalapa <Chriszalapa@gmail.com>
2020-12-01 19:11:34 -0800

    HomePage

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage'
 import EntryIndex from '../pages/EntryIndex';
 import UserIndex from '../pages/UserIndex';
 
@@ -7,5 +8,6 @@ export default (
   <Switch>
     <Route path = '/entries' component = {EntryIndex}/>
     <Route path = '/users' component = {UserIndex}/>
+    <Route path = '/' component = {HomePage}/>
   </Switch>
 )
